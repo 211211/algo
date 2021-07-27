0634698378b83ee9c04078419453d94619e67139
211211 <nhquan307@outlook.com>
2021-07-27 22:40:27 +0700

    leveltraversal.js

--- leveltraversal.js
+++ leveltraversal.js
@@ -30,3 +30,98 @@ var levelOrder = function (root) {
 //   [9,20],
 //   [15,7]
 // ]
+
+
+// var levelOrder = function (root) {
+//     const result = [];
+
+//     const queue = [root]
+//     let level = 0
+//     while(queue.length) {
+//       const size = queue.length
+//       for (let i = 0; i < size; i++) {
+//         const node = queue.shift()
+//         result.push(node.val)
+//         if (node.left) {
+//           queue.push(node.left)
+//         }
+
+//         if (node.right) {
+//           queue.push(node.right)
+//         }
+//       }
+
+//       level++
+//     }
+
+//     return result;
+// };
+
+var levelOrder = function (root) {
+  const result = [];
+
+  const dfs = (node) => {
+    if (node === null) {
+      return []
+    }
+    result.push(node.val)
+    dfs(node.left)
+    dfs(node.right)
+  }
+
+  dfs(root)
+  return result;
+};
+
+const input = {
+  val: 3,
+  left: {
+      val: 9,
+      left: {
+          val: 8,
+          left: null,
+          right: null,
+      },
+      right: {
+          val: 6,
+          left: null,
+          right: null,
+      },
+  },
+  right: {
+      val: 20,
+      left: {
+          val: 15,
+          left: null,
+          right: null,
+      },
+      right: {
+          val: 7,
+          left: null,
+          right: null,
+      },
+  },
+};
+
+
+console.log(levelOrder(input))
+
+
+var inOrder = function (root) {
+  const result = [];
+
+  const dfs = (node) => {
+    if (node === null) {
+      return []
+    }
+    
+    dfs(node.left)
+    result.push(node.val)
+    dfs(node.right)
+  }
+
+  dfs(root)
+  return result;
+};
+
+console.log(inOrder(input))
